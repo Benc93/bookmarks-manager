c38e01b60a3bc01de8542243f788c7fa2f8155f8
Ben Conway <bendavidconway@me.com>
2014-11-19 20:38:15 +0000

    submit new links

--- lib/server.rb
+++ lib/server.rb
@@ -6,8 +6,15 @@ require './lib/link'
 class BookmarkManager < Sinatra::Base
   
   get '/' do
-  @links = Link.all
-  erb :index
+    @links = Link.all
+    erb :index
+  end
+
+  post '/links' do
+    url = params["url"]
+    title = params["title"]
+    Link.create(:url => url, :title => title)
+    redirect to('/')
   end
 
 
