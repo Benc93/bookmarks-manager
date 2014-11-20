10c6858a94d7c60665381d3bcb995e308a279b7d
Ben Conway <bendavidconway@me.com>
2014-11-20 13:03:34 +0000

    managing duplicate emails

--- lib/server.rb
+++ lib/server.rb
@@ -64,11 +64,11 @@ class BookmarkManager < Sinatra::Base
               :password => params[:password],
               :password_confirmation => params[:password_confirmation])
    if @user.save
-     session[:user_id] = @user.id
-     redirect to('/')
+    session[:user_id] = @user.id
+    redirect to('/')
    else
-    flash[:notice] = "Sorry, your passwords don't match"
-     erb :"users/new"
+    flash.now[:errors] = @user.errors.full_messages
+    erb :"users/new"
    end
   end
 
