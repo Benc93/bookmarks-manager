bb989a42663afc86a301ba568249b1c0e6bc4ceb
Ben Conway <bendavidconway@me.com>
2014-11-20 12:03:10 +0000

    adding password confirmation funtionality

--- lib/server.rb
+++ lib/server.rb
@@ -56,10 +56,11 @@ class BookmarkManager < Sinatra::Base
   end
 
   post '/users' do
-    user = User.create(:email => params[:email],
-                     :password => params[:password])
-    session[:user_id] = user.id
-    redirect to('/')
+  user = User.create(:email => params[:email],
+              :password => params[:password],
+              :password_confirmation => params[:password_confirmation])
+  session[:user_id] = user.id
+  redirect to('/')
   end
 
 
