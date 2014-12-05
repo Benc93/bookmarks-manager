ccb9beff5bb68260ce5b88861a20d4479c04c0eb
Ben Conway <bendavidconway@me.com>
2014-12-05 22:30:41 +0000

    more restructuring, started forgotten passwords

--- controllers/users.rb
+++ controllers/users.rb
@@ -13,8 +13,42 @@ class BookmarkManager
      end
     end
 
-    get '/users/forgot_password' do 
-      'display a form where I can fill in my email'
-    end
+  get '/users/forgot_password' do
+    erb :"users/forgot_password"
+  end
+
+  post '/users/forgot_password' do
+    user = User.first(email: params[:email])
+    user.password_token = (1..64).map{('A'..'Z').to_a.sample}.join
+    user.save
+
+    
+    # @email = user.email
+    # @token = user.password_token
+    # mg_client = Mailgun::Client.new ENV['MAILGUN_API_KEY']
+    # message_params = {:from    => ENV['MAILGUN_FROM_ADDRESS'],
+    #                   # :to      => "#{@email}",
+    #                   :to => 'bendavidconway@me.com',
+    #                   :subject => 'Reset Password',
+    #                   :text    => "Please follow the following link to reset your password.\n http://vast-gorge-8099.herokuapp.com/users/reset_password/#{@token}"}
+    # mg_client.send_message 'sandbox9533715787bf49408a576e6f77424cf3.mailgun.org', message_params
+    # flash[:notice] = "Please check your email"
+    # redirect '/'
+  end
+
+  get '/users/reset_password/:token' do
+    @token = params[:token]
+    user = User.first(password_token: @token)
+    erb :"users/reset_password"
+  end
+
+  post '/users/reset_password' do
+    @token = params[:password_token]
+    user = User.first(password_token: @token)
+    user.update(password: params[:password], password_token: nil)
+
+    flash[:notice] = "Password successfully reset"
+    redirect '/'
+  end
 
 end
